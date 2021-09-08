76ea524920b68f450d37cd7ee05a0ca174387d4e
AnnaEleeva <anna_eleeva@yahoo.com>
2021-09-08 17:06:24 +0500

    ellipse and circle rendering

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // подключение express
 const express = require("express");
 const multer = require("multer");
-// const { getObjects } = require("./js/readFile.js");
-
 
 // создаем объект приложения
 const app = express();
+
 ///Внешние файлы
 const readUserFile = require("./js/readFile.js");
-// let allFigureObjects = readUserFile.allFigureObjects;
 
+// let allFigureObjects = readUserFile.allFigureObjects;
+const allFigureObjects = readUserFile.getObjects();
 
 const storageConfig = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -19,6 +19,7 @@ const storageConfig = multer.diskStorage({
         cb(null, file.fieldname);
     }
 });
+
 app.use(express.static(__dirname));
 app.use(multer({ storage: storageConfig }).single("figurefile.txt"));
 
@@ -32,12 +33,14 @@ app.get("/api/figures", function(req, res) {
     const content = allFigureObjects;
     res.send(content);
 });
+
 app.get("/figure", function(req, res) {
 
     // отправляем ответ
     res.sendFile(__dirname + '/html/figures.html');
     // res.render('/figure', { figures: allFigureObjects })
 });
+
 app.post("/upload", function(req, res, next) {
 
     let figurefile = req.file;
@@ -48,9 +51,7 @@ app.post("/upload", function(req, res, next) {
     //res.send("Файл загружен");
         res.sendFile(__dirname + '/html/filepost.html');
 })
-const allFigureObjects = readUserFile.getObjects();
 
-//console.log(allFigureObjects);
 
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000, () => { console.log("Server started"); });
--- js/figures.js
+++ js/figures.js
@@ -19,14 +19,13 @@ fetch('http://localhost:3000/api/figures')
                     paintRightTriangle(figureObject);
                     break;
                 case "30":
+
                     paintCircle(figureObject);
                     break;
                 case "31":
                     paintEllipse(figureObject);
                     break;
             }
-            // console.log(figureObject[2]);
-            // for (let figureParam of figureObject) {}
         }
 
         function paintRectangle(figureObject) {
@@ -36,25 +35,15 @@ fetch('http://localhost:3000/api/figures')
             let centerY = figureObject.y - figureObject.height / 2;
 
             ctx.save();
-            ctx.translate(canvas.width / 2, canvas.height / 2);
-            ctx.rotate(figureObject.angle * Math.PI / 180);
+            ctx.beginPath();
             ctx.fillStyle = figureObject.bgColour;
             ctx.strokeStyle = figureObject.conturColour;
-            ctx.strokeRect(centerX, centerY,
-                figureObject.width, figureObject.heigh);
-            ctx.fillRect(centerX, centerY,
-                figureObject.width, figureObject.height);
-            ctx.restore();
-
-            ctx.save();
             ctx.translate(canvas.width / 2, canvas.height / 2);
             ctx.rotate(figureObject.angle * Math.PI / 180);
-            // ctx.fillStyle = figureObject.bgColour;
-            ctx.strokeStyle = figureObject.conturColour;
-            ctx.strokeRect(centerX, centerY,
-                figureObject.width, figureObject.heigh);
-            // ctx.fillRect(centerX, centerY,
-            // figureObject.width, figureObject.height);
+            ctx.rect(centerX, centerY,
+                figureObject.width, figureObject.height);
+            ctx.fill();
+            ctx.stroke();
             ctx.restore();
         }
 
@@ -66,11 +55,34 @@ fetch('http://localhost:3000/api/figures')
 
         }
 
-        function paintCircle() {
+        function paintCircle(figureObject) {
+            const ctx = canvas.getContext("2d");
+            ctx.save();
+            ctx.translate(canvas.width / 2, canvas.height / 2);
+            ctx.beginPath();
+            ctx.strokeStyle = figureObject.conturColour;
+            ctx.lineWidth = 1;
+            ctx.arc(figureObject.x, figureObject.y,
+                figureObject.r, 0, 2 * Math.PI);
+            ctx.fillStyle = figureObject.bgColour;
+            ctx.fill();
+            ctx.stroke();
+            ctx.restore();
 
         }
 
-        function paintEllipse() {
-
+        function paintEllipse(figureObject) {
+            const ctx = canvas.getContext("2d");
+            ctx.save();
+            ctx.translate(canvas.width / 2, canvas.height / 2);
+            ctx.beginPath();
+            ctx.strokeStyle = figureObject.conturColour;
+            ctx.ellipse(figureObject.x, figureObject.y,
+                figureObject.r, figureObject.r2,
+                Math.PI / 180 * figureObject.angle, 0, 2 * Math.PI);
+            ctx.fillStyle = figureObject.bgColour;
+            ctx.fill();
+            ctx.stroke();
+            ctx.restore();
         }
     });
